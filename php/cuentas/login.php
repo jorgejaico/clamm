072f77e7f0dd1762e581478b92d8dcdbe5fdcb70
smjosel <33595.j23@fje.edu>
2016-05-05 12:09:58 +0200

    cosas a saco!

--- php/cuentas/login.php
+++ php/cuentas/login.php
@@ -8,25 +8,24 @@
 <head>
     <meta charset="utf8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>jQuery Dropdown Login Freebie | The Finished Box</title>
-    <link rel="stylesheet" href="../../css/styleLogin.css" />
+    <link rel="stylesheet" href="css/styleLogin.css" />
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js?ver=1.4.2"></script>
-    <script src="../../js/login.js"></script>
+    <script src="js/login.js"></script>
 </head>
 <?php
-	include("../conexion/conexion.proc.php");
+	include("php/conexion/conexion.proc.php");
 ?>
 
-
+	
 
 <div id="loginContainer">
-                <a href="#" id="loginButton"><span>Login</span><em></em></a>
+                <a href="#" id="loginButton"><span>Login</span></a>
                 <div style="clear:both"></div>
                 <div id="loginBox">                
-                    <form id="loginForm" name="f1" action="../conexion/login.proc.php" method="get">
+                    <form id="loginForm" name="f1" action="php/conexion/login.proc.php" method="get">
                         <fieldset id="body">
                             <fieldset>
-                                <label for="email">Email Address</label>
+                                <label for="email">Correo</label>
                                 <input type="text" name="mail" placeholder="Correo" maxlength="50">
                             </fieldset>
                             <fieldset>
@@ -34,9 +33,8 @@
     							<input type="password" name="pass" class="form-control" placeholder="Contraseña">
                             </fieldset>
                             <button type="submit" name="acce">Entrar</button>
-                            <label for="checkbox"><input type="checkbox" id="checkbox" />Remember me</label>
                         </fieldset>
-                        <span><a href="#">Forgot your password?</a></span>
+                        <span><a href="#">¿Olvidaste la contraseña?</a></span>
                     </form>
                 </div>
             </div>
