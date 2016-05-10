6aa3adafcf018985d379a80e537c8572f0e2d0e0
smjosel <33595.j23@fje.edu>
2016-05-10 12:31:54 +0200

    witget tweeter

---
+++ php/prueba_tweet.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>twitter</title>
+	
+    </head>
+    <body>
+     <a href="https://twitter.com/intent/tweet?screen_name=Moda_Clamm" class="twitter-mention-button" data-lang="es" data-size="large">Tweet a @Moda_Clamm</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<br><br>
+<a href="https://twitter.com/rockmoda" class="twitter-follow-button" data-show-count="false" data-lang="es" data-size="large" data-show-screen-name="false">Seguir a @Moda_Clamm</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<br><br>
+<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/search?q=Moda_Clamm" data-widget-id="729979586835054592">Tweets sobre Moda_Clamm</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    </body>
+</html>
