41442b55bff60de72a0593bf895faa9b70c4ca3c
smjosel <33595.j23@fje.edu>
2016-05-10 11:11:22 +0200

    articulo + tags

---
+++ php/prueba_articulo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<meta charset="utf-8">
+		<link rel="stylesheet" href="css/button.css" >
+    </head>
+    <body>
+    	<?php include ("php/conexion/conexion.proc.php"); 
+    	$sql = "SELECT tbl_articulo.*, tbl_tagarticulo.*, tbl_tags.* FROM tbl_articulo INNER JOIN tbl_tagarticulo ON id_articulo=articulo_tagarticulo INNER JOIN tbl_tags ON id_tag=tag_tagarticulo WHERE id_articulo=3";
+    	$datos_articulo= mysqli_query($con,$sql);
+    	if(mysqli_num_rows($datos_articulo) != 0){
+	    	$valor=mysqli_fetch_array($datos_articulo);
+	    		echo utf8_encode($valor['titulo_articulo'])."<br/>";
+	    		echo utf8_encode($valor['texto_articulo'])."<br/>";
+	    		while($valor=mysqli_fetch_array($datos_articulo)){
+	    			echo "<button class='button'>".$valor['nombre_tag']."</button>";
+	    		}
+
+	    }
+    	?>
+   	</body>
+</html>
