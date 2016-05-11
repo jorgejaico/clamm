97ad60fa18649a35d06b3ef01c3d9030dbcf8f02
jorgejaico <93186.joan23@fje.edu>
2016-05-11 11:23:02 +0200

    bloggers.php y css

---
+++ php/perfil/bloggers.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php session_start(); ?>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<title>CLAMM - Bloggers</title>
+		<!-- Bootstrap Core CSS -->
+		<link href="../../css/bootstrap.min.css" rel="stylesheet">
+		<!-- Custom Fonts -->
+		<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="../../css/patros.css" >
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+			<![endif]-->
+	</head>
+
+	<body data-spy="scroll">
+
+		<!-- Navigation -->
+
+	<?php
+		include ("../headerfooter/header.php");
+	?>
+
+		
+
+		<!-- Page Content -->
+		<section class="container blog">
+			<div class="row">
+		        <!-- Blog Column -->
+		        <div class="col-md-8">
+		            <h1 class="page-header sidebar-title">
+		                Bloggers
+		            </h1>
+		            <!-- First Blog Post -->
+		            <div class="row blogu">
+		                <div class="col-sm-4 col-md-4 ">
+		                    <div class="blog-thumb">
+		                        <a href="#">
+		                            <img class="img-responsive" src="../../images/blog-photo1.jpg" alt="photo">
+		                        </a>
+		                    </div>
+		                </div>
+		                <div class="col-sm-8 col-md-8">
+		                    <h2 class="blog-title">
+		                        <a href="#">Nombre de blogger</a>
+		                    </h2>
+		                    <p><i class="fa fa-calendar-o"></i>  Nº de posts
+		                        <span class="comments-padding"></span>
+		                        <i class="fa fa-comment"></i> Nº de Likes
+		                    </p>
+		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+		                </div>
+		            </div>
+		            <hr>
+		            <!-- Second Blog Post -->
+		            <div class="row">
+		                <div class="col-sm-4 col-md-4">
+		                    <div class="blog-thumb">
+		                        <a href="#">
+		                            <img class="img-responsive" src="../../images/blog-photo2.jpg" alt="photo">
+		                        </a>
+		                    </div>
+		                </div>
+		                <div class="col-sm-8 col-md-8">
+		                    <h2 class="blog-title">
+		                        <a href="#">Post title 2</a>
+		                    </h2>
+		                    <p><i class="fa fa-calendar-o"></i> August 28, 2013 
+		                       <span class="comments-padding"></span>
+		                       <i class="fa fa-comment"></i> 3 comments
+		                    </p>
+		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+		                </div>
+		            </div>
+		            <hr>
+		            <!-- Third Blog Post -->
+		            <div class="row">
+		                <div class="col-sm-4 col-md-4">
+		                    <div class="blog-thumb">
+		                        <a href="#">
+		                        <img class="img-responsive" src="../../images/blog-photo3.jpg" alt="photo">
+		                        </a>
+		                    </div>
+		                </div>
+		                <div class="col-sm-8 col-md-8">
+		                    <h2 class="blog-title">
+		                        <a href="#">Post title 3</a>
+		                    </h2>
+		                    <p><i class="fa fa-calendar-o"></i>  August 28, 2013 
+		                        <span class="comments-padding"></span>
+		                        <i class="fa fa-comment"></i> 1 comment
+		                    </p>
+		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+		                </div>
+		            </div>
+		            <hr>
+		            <!-- Fourth Blog Post -->
+		            <div class="row">
+		                <div class="col-sm-4 col-md-4">
+		                    <div class="blog-thumb">
+		                        <a href="#">
+		                            <img class="img-responsive" src="../../images/blog-photo1.jpg" alt="photo">
+		                        </a>
+		                    </div>
+		                </div>
+		                <div class="col-sm-8 col-md-8">
+		                    <h2 class="blog-title">
+		                        <a href="#">Post title 4</a>
+		                    </h2>
+		                    <p><i class="fa fa-calendar-o"></i> August 28, 2013 
+		                       <span class="comments-padding"></span>
+		                       <i class="fa fa-comment"></i> 3 comments
+		                    </p>
+		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+		                </div>
+		            </div>
+
+		             <hr>
+		            <!-- Fifth Blog Post -->
+		            <div class="row">
+		                <div class="col-sm-4 col-md-4">
+		                    <div class="blog-thumb">
+		                        <a href="#">
+		                            <img class="img-responsive" src="../../images/blog-photo2.jpg" alt="photo">
+		                        </a>
+		                    </div>
+		                </div>
+		                <div class="col-sm-8 col-md-8">
+		                    <h2 class="blog-title">
+		                        <a href="#">Post title 5</a>
+		                    </h2>
+		                    <p><i class="fa fa-calendar-o"></i> August 28, 2013 
+		                       <span class="comments-padding"></span>
+		                       <i class="fa fa-comment"></i> 3 comments
+		                    </p>
+		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+		                </div>
+		            </div>
+
+		             <hr>
+		            <!-- Six Blog Post -->
+		            <div class="row">
+		                <div class="col-sm-4 col-md-4">
+		                    <div class="blog-thumb">
+		                        <a href="#">
+		                            <img class="img-responsive" src="../../images/blog-photo3.jpg" alt="photo">
+		                        </a>
+		                    </div>
+		                </div>
+		                <div class="col-sm-8 col-md-8">
+		                    <h2 class="blog-title">
+		                        <a href="#">Post title 6</a>
+		                    </h2>
+		                    <p><i class="fa fa-calendar-o"></i> August 28, 2013 
+		                       <span class="comments-padding"></span>
+		                       <i class="fa fa-comment"></i> 3 comments
+		                    </p>
+		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+		                </div>
+		            </div>
+		            
+
+
+		            <hr>
+		            <div class="text-center"> 
+		                <ul class="pagination"> 
+		                    <li class="active"> <a href="#">1</a> </li> 
+		                    <li> <a href="#">2</a> </li> 
+		                    <li> <a href="#">3</a> </li> 
+		                    <li> <a href="#">4</a> </li> 
+		                    <li> <a href="#">5</a> </li> 
+		                    <li> <a href="#">Next</a> </li> 
+		                </ul> 
+		            </div>
+		        </div>
+		            <!-- Blog Sidebar Column -->
+		            <aside class="col-md-4 sidebar-padding">
+		                <div class="blog-sidebar">
+		                    <div class="input-group searchbar">
+		                        <input type="text" class="form-control searchbar" placeholder="Search for...">
+		                        <span class="input-group-btn">
+		                        <button class="btn btn-default" type="button">Search</button>
+		                        </span>
+		                    </div><!-- /input-group -->
+		                </div>
+		                
+		                <!-- Bloggers Destacados-->
+		                <div class="blog-sidebar">
+		                    <h4 class="sidebar-title"><i class="fa fa-align-left"></i> Bloggers destacados</h4>
+		                    <hr style="margin-bottom: 5px;">
+
+		                    <div class="media">
+		                        <a class="pull-left" href="#">
+		                            <img class="img-responsive media-object" src="../../images/blog1.jpg" alt="Media Object">
+		                        </a>
+		                        <div class="media-body">
+		                            <h4 class="media-heading"><a href="#">Post title 1</a></h4>
+		                            This is some sample text. This is some sample text. This is some sample text.
+		                        </div>
+		                    </div>
+
+		                    <div class="media">
+		                        <a class="pull-left" href="#">
+		                            <img class="img-responsive media-object" src="../../images/blog2.jpg" alt="Media Object">
+		                        </a>
+		                        <div class="media-body">
+		                            <h4 class="media-heading"><a href="#">Post title 2</a></h4>
+		                            This is some sample text. This is some sample text. This is some sample text.
+		                        </div>
+		                    </div>
+
+		                    <div class="media">
+		                        <a class="pull-left" href="#">
+		                            <img class="img-responsive media-object" src="../../images/blog3.jpg" alt="Media Object">
+		                        </a>
+		                        <div class="media-body">
+		                            <h4 class="media-heading"><a href="#">Post title 3</a></h4>
+		                            This is some sample text. This is some sample text. This is some sample text.
+		                        </div>
+		                    </div>
+		                    <div class="media">
+		                        <a class="pull-left" href="#">
+		                            <img class="img-responsive media-object" src="../../images/blog1.jpg" alt="Media Object">
+		                        </a>
+		                        <div class="media-body">
+		                            <h4 class="media-heading"><a href="#">Post title 4</a></h4>
+		                            This is some sample text. This is some sample text. This is some sample text.
+		                        </div>
+		                    </div>
+		                </div>
+
+		                <!-- Bloggers Recientes-->
+		                <div class="blog-sidebar">
+		                    <h4 class="sidebar-title"><i class="fa fa-align-left"></i> Bloggers Recientes</h4>
+		                    <hr style="margin-bottom: 5px;">
+
+		                    <div class="media">
+		                        <a class="pull-left" href="#">
+		                            <img class="img-responsive media-object" src="../../images/blog1.jpg" alt="Media Object">
+		                        </a>
+		                        <div class="media-body">
+		                            <h4 class="media-heading"><a href="#">Post title 1</a></h4>
+		                            This is some sample text. This is some sample text. This is some sample text.
+		                        </div>
+		                    </div>
+
+		                    <div class="media">
+		                        <a class="pull-left" href="#">
+		                            <img class="img-responsive media-object" src="../../images/blog2.jpg" alt="Media Object">
+		                        </a>
+		                        <div class="media-body">
+		                            <h4 class="media-heading"><a href="#">Post title 2</a></h4>
+		                            This is some sample text. This is some sample text. This is some sample text.
+		                        </div>
+		                    </div>
+
+		                    <div class="media">
+		                        <a class="pull-left" href="#">
+		                            <img class="img-responsive media-object" src="../../images/blog3.jpg" alt="Media Object">
+		                        </a>
+		                        <div class="media-body">
+		                            <h4 class="media-heading"><a href="#">Post title 3</a></h4>
+		                            This is some sample text. This is some sample text. This is some sample text.
+		                        </div>
+		                    </div>
+		                    <div class="media">
+		                        <a class="pull-left" href="#">
+		                            <img class="img-responsive media-object" src="../../images/blog1.jpg" alt="Media Object">
+		                        </a>
+		                        <div class="media-body">
+		                            <h4 class="media-heading"><a href="#">Post title 4</a></h4>
+		                            This is some sample text. This is some sample text. This is some sample text.
+		                        </div>
+		                    </div>
+		                </div>
+
+					</aside>
+				</div>
+		    </section>
+
+        
+		
+
+
+
+		<?php
+		include ("../headerfooter/footer.php");
+	?>
+
+
+
+
+
+        
+
+    </body>
+</html>
