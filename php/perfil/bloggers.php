a847aed1d069b2e96188acfc45f6d5efd8a882d2
SanchezEric <8706.joan23@fje.edu>
2016-05-12 11:03:53 +0200

    Bloggers.php leer contenido que falta:
    Igual que en la última subida de perfil faltan los enlaces a otras
    páginas

--- php/perfil/bloggers.php
+++ php/perfil/bloggers.php
@@ -1,7 +1,17 @@
 <!DOCTYPE html>
 <html lang="es">
-<?php session_start(); ?>
 	<head>
+		<?php
+		//Conexion BDD 
+			include("../conexion/conexion.proc.php");
+		//Consulta datos de usuario
+			$sql = "SELECT * FROM tbl_usuario INNER JOIN tbl_tipousuario ON tbl_usuario.tipousuario = tbl_tipousuario.id_tipoUsuario WHERE tbl_tipousuario.id_tipoUsuario = 4 ORDER BY Rand()";
+		//Consulta ultimos 4 usuarios registrados	
+			$sql2 ="SELECT * FROM tbl_usuario ORDER BY id_usuario DESC limit 4";
+			
+			$datos = mysqli_query($con, $sql);
+			$datos2 = mysqli_query($con, $sql2);
+		?>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -41,136 +51,41 @@
 		                Bloggers
 		            </h1>
 		            <!-- First Blog Post -->
-		            <div class="row blogu">
-		                <div class="col-sm-4 col-md-4 ">
-		                    <div class="blog-thumb">
-		                        <a href="#">
-		                            <img class="img-responsive" src="../../images/blog-photo1.jpg" alt="photo">
-		                        </a>
-		                    </div>
-		                </div>
-		                <div class="col-sm-8 col-md-8">
-		                    <h2 class="blog-title">
-		                        <a href="#">Nombre de blogger</a>
-		                    </h2>
-		                    <p><i class="fa fa-calendar-o"></i>  Nº de posts
-		                        <span class="comments-padding"></span>
-		                        <i class="fa fa-comment"></i> Nº de Likes
-		                    </p>
-		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-		                </div>
-		            </div>
-		            <hr>
-		            <!-- Second Blog Post -->
-		            <div class="row">
-		                <div class="col-sm-4 col-md-4">
-		                    <div class="blog-thumb">
-		                        <a href="#">
-		                            <img class="img-responsive" src="../../images/blog-photo2.jpg" alt="photo">
-		                        </a>
-		                    </div>
-		                </div>
-		                <div class="col-sm-8 col-md-8">
-		                    <h2 class="blog-title">
-		                        <a href="#">Post title 2</a>
-		                    </h2>
-		                    <p><i class="fa fa-calendar-o"></i> August 28, 2013 
-		                       <span class="comments-padding"></span>
-		                       <i class="fa fa-comment"></i> 3 comments
-		                    </p>
-		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-		                </div>
-		            </div>
-		            <hr>
-		            <!-- Third Blog Post -->
-		            <div class="row">
-		                <div class="col-sm-4 col-md-4">
-		                    <div class="blog-thumb">
-		                        <a href="#">
-		                        <img class="img-responsive" src="../../images/blog-photo3.jpg" alt="photo">
-		                        </a>
-		                    </div>
-		                </div>
-		                <div class="col-sm-8 col-md-8">
-		                    <h2 class="blog-title">
-		                        <a href="#">Post title 3</a>
-		                    </h2>
-		                    <p><i class="fa fa-calendar-o"></i>  August 28, 2013 
-		                        <span class="comments-padding"></span>
-		                        <i class="fa fa-comment"></i> 1 comment
-		                    </p>
-		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-		                </div>
-		            </div>
-		            <hr>
-		            <!-- Fourth Blog Post -->
-		            <div class="row">
-		                <div class="col-sm-4 col-md-4">
-		                    <div class="blog-thumb">
-		                        <a href="#">
-		                            <img class="img-responsive" src="../../images/blog-photo1.jpg" alt="photo">
-		                        </a>
-		                    </div>
-		                </div>
-		                <div class="col-sm-8 col-md-8">
-		                    <h2 class="blog-title">
-		                        <a href="#">Post title 4</a>
-		                    </h2>
-		                    <p><i class="fa fa-calendar-o"></i> August 28, 2013 
-		                       <span class="comments-padding"></span>
-		                       <i class="fa fa-comment"></i> 3 comments
-		                    </p>
-		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-		                </div>
-		            </div>
-
-		             <hr>
-		            <!-- Fifth Blog Post -->
-		            <div class="row">
-		                <div class="col-sm-4 col-md-4">
-		                    <div class="blog-thumb">
-		                        <a href="#">
-		                            <img class="img-responsive" src="../../images/blog-photo2.jpg" alt="photo">
-		                        </a>
-		                    </div>
-		                </div>
-		                <div class="col-sm-8 col-md-8">
-		                    <h2 class="blog-title">
-		                        <a href="#">Post title 5</a>
-		                    </h2>
-		                    <p><i class="fa fa-calendar-o"></i> August 28, 2013 
-		                       <span class="comments-padding"></span>
-		                       <i class="fa fa-comment"></i> 3 comments
-		                    </p>
-		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-		                </div>
-		            </div>
-
-		             <hr>
-		            <!-- Six Blog Post -->
-		            <div class="row">
-		                <div class="col-sm-4 col-md-4">
-		                    <div class="blog-thumb">
-		                        <a href="#">
-		                            <img class="img-responsive" src="../../images/blog-photo3.jpg" alt="photo">
-		                        </a>
-		                    </div>
-		                </div>
-		                <div class="col-sm-8 col-md-8">
-		                    <h2 class="blog-title">
-		                        <a href="#">Post title 6</a>
-		                    </h2>
-		                    <p><i class="fa fa-calendar-o"></i> August 28, 2013 
-		                       <span class="comments-padding"></span>
-		                       <i class="fa fa-comment"></i> 3 comments
-		                    </p>
-		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-		                </div>
-		            </div>
-		            
-
-
-		            <hr>
+		            <?php
+		            while ($prod = mysqli_fetch_array($datos)){
+		            	// Consulta numero total de posts
+				            $sql_posts = "SELECT COUNT(id_articulo) FROM tbl_articulo WHERE usuario_articulo = $prod[id_usuario]";
+				            $datos_posts = mysqli_query($con, $sql_posts);
+				            $prod_posts = mysqli_fetch_array($datos_posts);
+		                // Consulta total de likes
+				            $sql_likes = "SELECT tbl_usuario.id_usuario, tbl_articulo.id_articulo, COUNT(tbl_likes.id_likes) AS num_likes FROM tbl_usuario INNER JOIN tbl_articulo ON tbl_articulo.usuario_articulo = tbl_usuario.id_usuario INNER JOIN tbl_likes ON tbl_likes.articulo_likes = tbl_articulo.id_articulo WHERE tbl_usuario.id_usuario = $prod[id_usuario]";
+				            $datos_likes = mysqli_query($con, $sql_likes);
+				            $prod_likes = mysqli_fetch_array($datos_likes);        	
+		            	?>
+			            <div class="row blogu">
+			                <div class="col-sm-4 col-md-4 ">
+			                    <div class="blog-thumb">
+			                        <a href="#">
+			                            <img src="../../usuarios/<?php echo $prod['usuario'] ?>/<?php echo $prod['img_usuario'] ?>" class="img-responsive" alt="photo">
+			                        </a>
+			                    </div>
+			                </div>
+			                <div class="col-sm-8 col-md-8">
+			                    <h2 class="blog-title">
+			                        <a href="perfil.php"><?php echo $prod['usuario'];?></a>
+			                    </h2>
+			                    <p><i class="fa fa-calendar-o"></i>  Nº de posts: <b><?php echo $prod_posts['COUNT(id_articulo)']; ?></b>
+			                        <span class="comments-padding"></span>
+			                        <i class="fa fa-comment"></i> Nº de Likes: <b><?php echo $prod_likes['num_likes'];?></b>
+			                    </p>
+			                    <p><?php echo $prod['bio_usuario'];?></p>
+			                </div>
+			            </div>
+			            <hr>
+		            	<?php
+		            }
+		            ?>
+		           <!-- Paginacion -->
 		            <div class="text-center"> 
 		                <ul class="pagination"> 
 		                    <li class="active"> <a href="#">1</a> </li> 
@@ -194,7 +109,8 @@
 		                </div>
 		                
 		                <!-- Bloggers Destacados-->
-		                <div class="blog-sidebar">
+		        <!--  Descomentar cuando tengamos la sentencia correcta-->
+                  <!-- 	<div class="blog-sidebar">
 		                    <h4 class="sidebar-title"><i class="fa fa-align-left"></i> Bloggers destacados</h4>
 		                    <hr style="margin-bottom: 5px;">
 
@@ -236,51 +152,34 @@
 		                            This is some sample text. This is some sample text. This is some sample text.
 		                        </div>
 		                    </div>
-		                </div>
+		                </div> -->
 
 		                <!-- Bloggers Recientes-->
 		                <div class="blog-sidebar">
 		                    <h4 class="sidebar-title"><i class="fa fa-align-left"></i> Bloggers Recientes</h4>
 		                    <hr style="margin-bottom: 5px;">
-
+		                    <?php
+		                while ($prod2 = mysqli_fetch_array($datos2)){
+		                	?>
 		                    <div class="media">
-		                        <a class="pull-left" href="#">
-		                            <img class="img-responsive media-object" src="../../images/blog1.jpg" alt="Media Object">
+		                    	
+
+		                        <a class="pull-left" href="perfil.php" >
+		                        	
+		                            <img class="img-responsive media-object" src="../../usuarios/<?php echo $prod['usuario'] ?>/<?php echo $prod['img_usuario'] ?>" alt="Media Object">
 		                        </a>
 		                        <div class="media-body">
-		                            <h4 class="media-heading"><a href="#">Post title 1</a></h4>
-		                            This is some sample text. This is some sample text. This is some sample text.
-		                        </div>
-		                    </div>
-
-		                    <div class="media">
-		                        <a class="pull-left" href="#">
-		                            <img class="img-responsive media-object" src="../../images/blog2.jpg" alt="Media Object">
-		                        </a>
-		                        <div class="media-body">
-		                            <h4 class="media-heading"><a href="#">Post title 2</a></h4>
-		                            This is some sample text. This is some sample text. This is some sample text.
+		                            <h4 class="media-heading"><a href="perfil.php"><?php echo $prod2['usuario']; ?></a></h4>
+		                            <?php
+		                            	echo $prod2['bio_usuario'];
+		                            ?>
 		                        </div>
 		                    </div>
 
-		                    <div class="media">
-		                        <a class="pull-left" href="#">
-		                            <img class="img-responsive media-object" src="../../images/blog3.jpg" alt="Media Object">
-		                        </a>
-		                        <div class="media-body">
-		                            <h4 class="media-heading"><a href="#">Post title 3</a></h4>
-		                            This is some sample text. This is some sample text. This is some sample text.
-		                        </div>
-		                    </div>
-		                    <div class="media">
-		                        <a class="pull-left" href="#">
-		                            <img class="img-responsive media-object" src="../../images/blog1.jpg" alt="Media Object">
-		                        </a>
-		                        <div class="media-body">
-		                            <h4 class="media-heading"><a href="#">Post title 4</a></h4>
-		                            This is some sample text. This is some sample text. This is some sample text.
-		                        </div>
-		                    </div>
+		                    <?php
+		                }
+		                    ?>
+
 		                </div>
 
 					</aside>
