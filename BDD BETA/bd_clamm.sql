6eb1272a3180cf77efdc80ffad174d4098e9b880
SanchezEric <8706.joan23@fje.edu>
2016-05-17 20:14:01 +0200

    Versión revisada de la BDD

--- BDD BETA/bd_clamm.sql
+++ BDD BETA/bd_clamm.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-05-2016 a las 13:26:46
+-- Tiempo de generación: 17-05-2016 a las 20:13:28
 -- Versión del servidor: 5.6.26
 -- Versión de PHP: 5.6.12
 
@@ -30,7 +30,7 @@ USE `bd_clamm`;
 --
 -- Estructura de tabla para la tabla `tbl_anuncio`
 --
--- Creación: 11-05-2016 a las 07:16:03
+-- Creación: 17-05-2016 a las 17:36:27
 --
 
 DROP TABLE IF EXISTS `tbl_anuncio`;
@@ -40,8 +40,9 @@ CREATE TABLE IF NOT EXISTS `tbl_anuncio` (
   `texto_anuncio` varchar(250) COLLATE utf8_bin NOT NULL,
   `icono_anuncio` varchar(20) COLLATE utf8_bin NOT NULL,
   `imagen_anuncio` varchar(20) COLLATE utf8_bin NOT NULL,
-  `usuario_anuncio` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `usuario_anuncio` int(11) NOT NULL,
+  `activo_anuncio` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- RELACIONES PARA LA TABLA `tbl_anuncio`:
@@ -49,55 +50,64 @@ CREATE TABLE IF NOT EXISTS `tbl_anuncio` (
 --       `tbl_usuario` -> `id_usuario`
 --
 
+--
+-- Volcado de datos para la tabla `tbl_anuncio`
+--
+
+INSERT INTO `tbl_anuncio` (`id_anuncio`, `titulo_anuncio`, `texto_anuncio`, `icono_anuncio`, `imagen_anuncio`, `usuario_anuncio`, `activo_anuncio`) VALUES
+(1, 'afsadgs', 'sagh', 'fgfdsfg', 'fdfgsf', 1, 0),
+(2, 'fasdfsdf', 'sadfsdf', 'sdfasfd', 'asfsadf', 1, 0),
+(3, 'asfdsdf', 'asfsad', 'fsdfadsfads', 'fsdfsd', 1, 1),
+(4, 'asfdasdf', 'asfdadsf', 'asdf', 'asdfasf', 2, 1),
+(5, 'asfadsf', 'adsdfasf', 'asdfasdf', 'asdfasdf', 2, 1);
+
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `tbl_articulo`
 --
--- Creación: 11-05-2016 a las 09:00:41
+-- Creación: 17-05-2016 a las 13:50:35
 --
 
 DROP TABLE IF EXISTS `tbl_articulo`;
 CREATE TABLE IF NOT EXISTS `tbl_articulo` (
   `id_articulo` int(11) NOT NULL,
-  `titulo_articulo` varchar(30) COLLATE utf8_bin NOT NULL,
+  `titulo_articulo` varchar(60) COLLATE utf8_bin NOT NULL,
   `texto_articulo` text COLLATE utf8_bin NOT NULL,
   `usuario_articulo` int(11) NOT NULL,
   `portada_articulo` varchar(20) COLLATE utf8_bin DEFAULT NULL,
-  `menu_articulo` int(11) NOT NULL,
-  `fecha_articulo` datetime NOT NULL
+  `fecha_articulo` datetime NOT NULL,
+  `tipo_articulo` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- RELACIONES PARA LA TABLA `tbl_articulo`:
 --   `usuario_articulo`
 --       `tbl_usuario` -> `id_usuario`
---   `menu_articulo`
---       `tbl_menu` -> `id_menu`
 --
 
 --
 -- Volcado de datos para la tabla `tbl_articulo`
 --
 
-INSERT INTO `tbl_articulo` (`id_articulo`, `titulo_articulo`, `texto_articulo`, `usuario_articulo`, `portada_articulo`, `menu_articulo`, `fecha_articulo`) VALUES
-(11, 'art1', 'gdsrtaETYEWQr', 2, 'img1.png', 1, '2016-05-17 00:00:00'),
-(12, 'art2', 'dfsfr', 2, 'img1.png', 1, '2016-05-27 00:00:00'),
-(13, 'art3', 'hetrwtf', 2, 'img2.png', 1, '2016-05-22 00:00:00'),
-(14, 'art4', 'sfra', 2, 'img2.png', 1, '2016-05-19 00:00:00'),
-(15, 'art5', 'fdsafsdf', 2, 'img3.png', 1, '2016-05-20 00:00:00'),
-(16, 'art6', 'fasdfs', 2, 'img3.png', 1, '2016-05-10 04:00:00'),
-(17, 'art7', 'asdfsaf', 2, 'img4.png', 1, '2016-05-10 10:00:00'),
-(18, 'art8', 'asfdsda', 2, 'img4.png', 1, '2016-05-06 00:00:00'),
-(19, 'art9', 'asdfsd', 1, 'img5.png', 1, '2016-04-11 00:00:00'),
-(20, 'art10', 'asfddsaf', 2, 'img5.png', 1, '2016-05-18 00:00:00');
+INSERT INTO `tbl_articulo` (`id_articulo`, `titulo_articulo`, `texto_articulo`, `usuario_articulo`, `portada_articulo`, `fecha_articulo`, `tipo_articulo`) VALUES
+(11, 'Zara amplía su tallaje hasta la XXL', 'Zara ha ampliado su tallaje hasta la XXL esta semana y ya se pueden adquirir dichas prendas, que figuran entre las últimas novedades, en su página web. De momento, no toda la colección primavera-verano se ha diseñado en esta talla, pero podría ser cuestión de tiempo. La respuesta de la marca de Inditex a la longeva petición de sus clientas de ampliar el tallaje llega en la misma semana que un informe sobre los anuncios de esta temporada desvelaba que solo el 1,4% de las modelos superaban la talla 40. Mango es una de las primeras firmas españolas que dedicó un espacio a sus clientas con más tallaje creando la línea Violeta by Mango, que ya tiene incluso tienda propia.  \n\nEste cambio de ruta podría significar una nueva vuelta de hoja del mundo de la moda que, poco a poco, comienza a empoderar de nuevo las curvas como ya hiciera en los noventa. Modelos como Ashley Graham —quien utiliza sus redes sociales para demostrar que una top no tiene ninguna necesidad de un cuerpo huesudo— o Candice Huffine han revolucionado la industria y ya protagonizan campañas de lencería y moda de baño. ', 2, 'img1.png', '2016-05-17 00:00:00', 0),
+(12, 'La corbata ya no es necesaria', 'La reputada periodista de moda Vanessa Friedman reflexionaba esta semana en The New York Timessobre el menguante uso de la corbata en la clase política mundial. Mientras el magnate y candidato republicano a la Casa Blanca Donald Trump se aferra con obstinación a sus corbatas rojas de Brioni, el presidente Barack Obama se la quita a la mínima ocasión. Políticos de todos los colores la usan cada vez menos. Símbolo de los hombres de bien y de las cosas serias, la desaparición de la corbata en el uniforme político no es un tema baladí. Si el incremento del uso del esmalte de uñas es un claro indicador de tiempos de crisis, ¿qué significados esconderá el destierro de este complemento sartorial por parte de los políticos? Economía sin corbatas se puede leer en la cubierta de la edición española de uno de los últimos libros de Yanis Varoufakis, el nuevo héroe de la izquierda. El ingenioso titular parecía anticipar también la nueva era de la política sin corbatas. Y es que durante las últimas elecciones en España, la corbata y la indumentaria de los nuevos candidatos ha ocupado más espacio que nunca en la prensa. Estilistas y personal shoppers de todo pelaje intentaban desencriptar los significados y mensajes por el uso y no uso de determinadas prendas por parte de los políticos y sus asesores. Al inicio de campaña cada uno parecía tener una etiqueta clara: Pablo Iglesias y sus ya famosas camisas compradas en Alcampo, en la línea desenfadada de sus compañeros de la marea lila como el griego Alexis Tsipras; Albert Rivera y sus inmaculados trajes chaqueta y corbatas estrechitas; Pedro Sánchez en busca de una corrección simpática con un uso intermitente de la corbata; y Rajoy, como Trump, aferrado a ellas de toda la vida.', 2, 'img1.png', '2016-05-27 00:00:00', 0),
+(13, 'Beyoncé, acusada de fabricar con mano de obra esclava ', 'Beyoncé quiere que su línea de ropa deportiva, Ivy Park, “apoye” e “inspire” a las mujeres. El objetivo probablemente solo se cumple para las que pagan dinero por las prendas en Topshop y Nordstrom. Quienes las fabrican lo hacen en talleres clandestinos de Sri Lanka y cobran 5,46 euros al día. Necesitarían trabajar más de un mes para comprarse uno de los leggings que la diva vende por 145 euros. Según una investigación del diario británico The Sun, las costureras son mujeres jóvenes y pobres, originarias del campo, que solo pueden permitirse vivir en residencias atestadas y que necesitan laborar más de 60 horas a la semana para cubrir sus gastos Una chica de 22 años, que opera una máquina de coser, contó al periódico que vive en una residencia de 100 habitaciones cerca de la fábrica en la ciudad de Katunayake.“Todo lo que hago es trabajar, dormir, trabajar, dormir”, dice. La empleada asegura que no puede salir adelante con su salario de 110 euros al mes. La cantidad es poco más de la mitad del salario medio en Sri Lanka, que se calcula en 207 euros. La joven trabaja casi 10 horas al día, de lunes a viernes, con un descanso de 30 minutos para comer. Tiene que coser también los sábados y hacer horas extra entre semana para llegar a fin de mes. La chica, hija de un granjero de una aldea a más de 300 kilómetros de distancia, comparte una habitación de tres metros por tres metros con su hermana de 19 años. Cada una paga 24 euros al mes. “No tenemos nuestra propia cocina o ducha; solo es un pequeño dormitorio”, explicó a The Sun. “Tenemos que compartir el barracón de duchas con los hombres, así que no hay mucha privacidad. Es impactante y muchas de las mujeres tiene mucho miedo”, añadió.', 2, 'img2.png', '2016-05-22 00:00:00', 0),
+(14, 'art4', 'sfra', 2, 'img2.png', '2016-05-19 00:00:00', 0),
+(15, 'art5', 'fdsafsdf', 2, 'img3.png', '2016-05-20 00:00:00', 0),
+(16, 'Historia de amor entre un vegano y una carnívora', 'Hace poco más de un mes decidí hacerme vegano. Llevaba bastante tiempo pensándolo, pero me animé del todo al ver que muchos de mis amigos optaban por dejar de comer carne. Es que lo de darle al tofu se ha puesto muy de moda últimamente en Malasaña. Pero no es mi caso, para nada. Lo mío fue más por conciencia, que cuando te enteras de cómo funciona la producción de carne y pescado da reparo. Así que después de una cena con mis amigos en la que todos me pusieron cara rara por ser el único que no se pedía la hamburguesa vegana, decidí dejar de hincarle el diente a los animales.\n\nLo hice en plan radical, de un día para otro, y hasta lo anuncié en mi Facebook. Jamás había conseguido tantos me gusta en un estado. Menos de mi madre, que me puso un emoticono lloroso. Ella dice que los veganos sólo comen guarnición y me iba a pasar todo el día con gripe por falta de nutrientes, pero mi médico de cabecera, la doctora Romaní, no lo ve así: “Los médicos recomendamos una dieta completa, y una vegana lo es añadiendo un suplemento de vitamina B12, que es lo que puede faltar. A nivel cardiovascular es muy favorable, reduce el colesterol y con ellas las posibilidades de tener un infarto. No es casualidad que el presidente de la Asociación Americana de Cardiología, el Dr. Kim Williams, sea vegano”.\n\nQuizá habría que explica lo que es ser vegano. Según la Asociación Vegana Española (UVE), “es una alternativa ética al consumo y a la dependencia de productos no adaptados a las necesidades físicas y espirituales del ser humano, como la carne, el pescado, los lácteos, los huevos, la miel, los productos derivados de los animales y otros artículos de origen animal como el cuero y las pieles”. ', 4, 'img3.png', '2016-05-10 04:00:00', 1),
+(17, 'art7', 'asdfsaf', 4, 'img4.png', '2016-05-10 10:00:00', 1),
+(18, 'art8', 'asfdsda', 4, 'img4.png', '2016-05-06 00:00:00', 1),
+(19, 'art9', 'asdfsd', 4, 'img5.png', '2016-04-11 00:00:00', 1),
+(20, 'art10', 'asfddsaf', 4, 'img5.png', '2016-05-18 00:00:00', 1);
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `tbl_comentario`
 --
--- Creación: 11-05-2016 a las 07:16:03
+-- Creación: 17-05-2016 a las 13:31:34
 --
 
 DROP TABLE IF EXISTS `tbl_comentario`;
@@ -119,7 +129,7 @@ CREATE TABLE IF NOT EXISTS `tbl_comentario` (
 --
 -- Estructura de tabla para la tabla `tbl_imgarticulo`
 --
--- Creación: 11-05-2016 a las 07:16:03
+-- Creación: 17-05-2016 a las 13:31:34
 --
 
 DROP TABLE IF EXISTS `tbl_imgarticulo`;
@@ -127,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `tbl_imgarticulo` (
   `id_imgarticulo` int(11) NOT NULL,
   `nombre_imgarticulo` varchar(25) COLLATE utf8_bin NOT NULL,
   `articulo_imgarticulo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- RELACIONES PARA LA TABLA `tbl_imgarticulo`:
@@ -135,12 +145,20 @@ CREATE TABLE IF NOT EXISTS `tbl_imgarticulo` (
 --       `tbl_articulo` -> `id_articulo`
 --
 
+--
+-- Volcado de datos para la tabla `tbl_imgarticulo`
+--
+
+INSERT INTO `tbl_imgarticulo` (`id_imgarticulo`, `nombre_imgarticulo`, `articulo_imgarticulo`) VALUES
+(1, 'j,hmgncgh', 20),
+(2, 'bvcfghjk,', 20);
+
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `tbl_likes`
 --
--- Creación: 11-05-2016 a las 07:20:10
+-- Creación: 17-05-2016 a las 13:31:34
 --
 
 DROP TABLE IF EXISTS `tbl_likes`;
@@ -152,8 +170,6 @@ CREATE TABLE IF NOT EXISTS `tbl_likes` (
 
 --
 -- RELACIONES PARA LA TABLA `tbl_likes`:
---   `articulo_likes`
---       `tbl_articulo` -> `id_articulo`
 --
 
 --
@@ -175,35 +191,9 @@ INSERT INTO `tbl_likes` (`id_likes`, `articulo_likes`, `usuario_likes`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tbl_menu`
---
--- Creación: 11-05-2016 a las 07:16:03
---
-
-DROP TABLE IF EXISTS `tbl_menu`;
-CREATE TABLE IF NOT EXISTS `tbl_menu` (
-  `id_menu` int(11) NOT NULL,
-  `nombre_menu` varchar(25) COLLATE utf8_bin NOT NULL,
-  `padre_menu` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- RELACIONES PARA LA TABLA `tbl_menu`:
---
-
---
--- Volcado de datos para la tabla `tbl_menu`
---
-
-INSERT INTO `tbl_menu` (`id_menu`, `nombre_menu`, `padre_menu`) VALUES
-(1, 'menu', 1);
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `tbl_publicacion`
 --
--- Creación: 11-05-2016 a las 07:16:03
+-- Creación: 17-05-2016 a las 13:31:34
 --
 
 DROP TABLE IF EXISTS `tbl_publicacion`;
@@ -226,7 +216,7 @@ CREATE TABLE IF NOT EXISTS `tbl_publicacion` (
 --
 -- Estructura de tabla para la tabla `tbl_tagarticulo`
 --
--- Creación: 11-05-2016 a las 07:16:03
+-- Creación: 17-05-2016 a las 13:31:34
 --
 
 DROP TABLE IF EXISTS `tbl_tagarticulo`;
@@ -249,7 +239,7 @@ CREATE TABLE IF NOT EXISTS `tbl_tagarticulo` (
 --
 -- Estructura de tabla para la tabla `tbl_tags`
 --
--- Creación: 11-05-2016 a las 07:16:03
+-- Creación: 17-05-2016 a las 13:31:34
 --
 
 DROP TABLE IF EXISTS `tbl_tags`;
@@ -267,7 +257,7 @@ CREATE TABLE IF NOT EXISTS `tbl_tags` (
 --
 -- Estructura de tabla para la tabla `tbl_tipousuario`
 --
--- Creación: 11-05-2016 a las 07:16:03
+-- Creación: 17-05-2016 a las 13:31:34
 --
 
 DROP TABLE IF EXISTS `tbl_tipousuario`;
@@ -297,7 +287,7 @@ INSERT INTO `tbl_tipousuario` (`id_tipoUsuario`, `nombre_tipousuario`, `desc_tip
 --
 -- Estructura de tabla para la tabla `tbl_usuario`
 --
--- Creación: 11-05-2016 a las 07:19:14
+-- Creación: 17-05-2016 a las 13:31:34
 --
 
 DROP TABLE IF EXISTS `tbl_usuario`;
@@ -345,8 +335,7 @@ ALTER TABLE `tbl_anuncio`
 --
 ALTER TABLE `tbl_articulo`
   ADD PRIMARY KEY (`id_articulo`),
-  ADD KEY `usuario_articulo` (`usuario_articulo`),
-  ADD KEY `menu_articulo` (`menu_articulo`);
+  ADD KEY `usuario_articulo` (`usuario_articulo`);
 
 --
 -- Indices de la tabla `tbl_comentario`
@@ -369,12 +358,6 @@ ALTER TABLE `tbl_imgarticulo`
 ALTER TABLE `tbl_likes`
   ADD PRIMARY KEY (`id_likes`),
   ADD KEY `articulo_like` (`articulo_likes`);
-
---
--- Indices de la tabla `tbl_menu`
---
-ALTER TABLE `tbl_menu`
-  ADD PRIMARY KEY (`id_menu`);
 
 --
 -- Indices de la tabla `tbl_publicacion`
@@ -419,7 +402,7 @@ ALTER TABLE `tbl_usuario`
 -- AUTO_INCREMENT de la tabla `tbl_anuncio`
 --
 ALTER TABLE `tbl_anuncio`
-  MODIFY `id_anuncio` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_anuncio` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT de la tabla `tbl_articulo`
 --
@@ -434,17 +417,12 @@ ALTER TABLE `tbl_comentario`
 -- AUTO_INCREMENT de la tabla `tbl_imgarticulo`
 --
 ALTER TABLE `tbl_imgarticulo`
-  MODIFY `id_imgarticulo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_imgarticulo` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `tbl_likes`
 --
 ALTER TABLE `tbl_likes`
   MODIFY `id_likes` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
---
--- AUTO_INCREMENT de la tabla `tbl_menu`
---
-ALTER TABLE `tbl_menu`
-  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `tbl_tagarticulo`
 --
@@ -479,8 +457,7 @@ ALTER TABLE `tbl_anuncio`
 -- Filtros para la tabla `tbl_articulo`
 --
 ALTER TABLE `tbl_articulo`
-  ADD CONSTRAINT `tbl_articulo_ibfk_1` FOREIGN KEY (`usuario_articulo`) REFERENCES `tbl_usuario` (`id_usuario`),
-  ADD CONSTRAINT `tbl_articulo_ibfk_2` FOREIGN KEY (`menu_articulo`) REFERENCES `tbl_menu` (`id_menu`);
+  ADD CONSTRAINT `tbl_articulo_ibfk_1` FOREIGN KEY (`usuario_articulo`) REFERENCES `tbl_usuario` (`id_usuario`);
 
 --
 -- Filtros para la tabla `tbl_comentario`
