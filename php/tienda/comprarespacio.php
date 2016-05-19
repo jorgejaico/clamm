7ae990e5c11561c067654d527727e9de062de4bf
SanchezEric <8706.joan23@fje.edu>
2016-05-19 13:32:45 +0200

    Por hacer Completamente

---
+++ php/tienda/comprarespacio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php session_start(); ?>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<form>
+				<p>Numero de Anuncios:</p>
+				  <input type="radio" name="num_anuncio" value="1" checked> Uno<br>
+				  <input type="radio" name="num_anuncio" value="2"> Dos<br>
+				  <input type="radio" name="num_anuncio" value="4"> Cuatro<br>
+				  <input type="radio" name="num_anuncio" value="otracant"> Otra Cantidad
+				<p>Numero de Meses</p>
+				  <input type="radio" name="num_mes" value="1" checked> Uno<br>
+				  <input type="radio" name="num_mes" value="2"> Dos<br>
+				  <input type="radio" name="num_mes" value="4"> Cuatro<br>
+				  <input type="radio" name="num_mes" value="otracant"> Otra Cantidad
+				  
+		</form>
+	</body>
+</html>
