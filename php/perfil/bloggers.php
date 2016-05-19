3faa5408d708b70d6b1589fb39d094e332635969
SanchezEric <8706.joan23@fje.edu>
2016-05-19 13:29:09 +0200

    Bloggers con Paginación

--- php/perfil/bloggers.php
+++ php/perfil/bloggers.php
@@ -11,7 +11,50 @@
 			
 			$datos = mysqli_query($con, $sql);
 			$datos2 = mysqli_query($con, $sql2);
-		?>
+
+
+
+	//Inicio contenido de paginación Header
+				//Cantidad de resultados por página (debe ser INT, no string/varchar)
+			$cantidad_resultados_por_pagina = 5;
+
+			//Comprueba si está seteado el GET de HTTP
+			if (isset($_GET["pagina"])) {
+
+				//Si el GET de HTTP SÍ es una string / cadena, procede
+				if (is_string($_GET["pagina"])) {
+
+					//Si la string es numérica, define la variable 'pagina'
+					 if (is_numeric($_GET["pagina"])) {
+
+						 //Si la petición desde la paginación es la página uno
+						 //en lugar de ir a 'index.php?pagina=1' se iría directamente a 'index.php'
+						 if ($_GET["pagina"] == 1) {
+							 header("Location: bloggers.php");
+							 die();
+						 } else { //Si la petición desde la paginación no es para ir a la pagina 1, va a la que sea
+							 $pagina = $_GET["pagina"];
+						};
+
+					 } else { //Si la string no es numérica, redirige al index (por ejemplo: index.php?pagina=AAA)
+						 header("Location: bloggers.php");
+						die();
+					 };
+				};
+
+			} else { //Si el GET de HTTP no está seteado, lleva a la primera página (puede ser cambiado al index.php o lo que sea)
+				$pagina = 1;
+			};
+
+			//Define el número 0 para empezar a paginar multiplicado por la cantidad de resultados por página
+			$empezar_desde = ($pagina-1) * $cantidad_resultados_por_pagina;
+	//Fin Contenido Paginación Header
+
+?>
+
+
+
+
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -38,6 +81,29 @@
 
 	<?php
 		include ("../headerfooter/header.php");
+
+	// Paginación Inicio BODY
+		//Obtiene TODO de la tabla
+		$obtener_todo_BD = "SELECT * FROM tbl_usuario WHERE `tipousuario` = 4";
+
+		//Realiza la consulta
+		$consulta_todo = mysqli_query($con, $obtener_todo_BD);
+
+		//Cuenta el número total de registros
+		$total_registros = mysqli_num_rows($consulta_todo);
+
+		//Obtiene el total de páginas existentes
+		$total_paginas = ceil($total_registros / $cantidad_resultados_por_pagina);
+//Realiza la consulta en el orden de ID ascendente (cambiar "id" por, por ejemplo, "nombre" o "edad", alfabéticamente, etc.)
+//Limitada por la cantidad de cantidad por página
+$consulta_resultados = mysqli_query($con, "
+SELECT tbl_usuario.*, tbl_articulo.*, COUNT(tbl_likes.id_likes) AS num_likes 
+		FROM tbl_usuario LEFT JOIN tbl_articulo ON tbl_articulo.usuario_articulo = tbl_usuario.id_usuario 
+						LEFT JOIN tbl_likes ON tbl_likes.articulo_likes = tbl_articulo.id_articulo 
+						WHERE tbl_usuario.tipousuario = 4 GROUP BY tbl_usuario.id_usuario 
+						ORDER by COUNT(tbl_likes.id_likes) DESC LIMIT $empezar_desde, $cantidad_resultados_por_pagina");
+	// Paginación Final Inicio BODY
+
 	?>
 
 		
@@ -52,7 +118,7 @@
 		            </h1>
 		            <!-- First Blog Post -->
 		            <?php
-		            while ($prod = mysqli_fetch_array($datos)){
+		            while ($prod = mysqli_fetch_array($consulta_resultados)){
 		            	// Consulta numero total de posts
 				            $sql_posts = "SELECT COUNT(id_articulo) FROM tbl_articulo WHERE usuario_articulo = $prod[id_usuario]";
 				            $datos_posts = mysqli_query($con, $sql_posts);
@@ -87,14 +153,20 @@
 		            ?>
 		           <!-- Paginacion -->
 		            <div class="text-center"> 
-		                <ul class="pagination"> 
-		                    <li class="active"> <a href="#">1</a> </li> 
-		                    <li> <a href="#">2</a> </li> 
-		                    <li> <a href="#">3</a> </li> 
-		                    <li> <a href="#">4</a> </li> 
-		                    <li> <a href="#">5</a> </li> 
-		                    <li> <a href="#">Next</a> </li> 
-		                </ul> 
+						<?php
+						//Crea un bucle donde $i es igual 1, y hasta que $i sea menor o igual a X, a sumar (1, 2, 3, etc.)
+						//Nota: X = $total_paginas
+						for ($i=1; $i<=$total_paginas; $i++) {
+							//En el bucle, muestra la paginación
+							 echo "<div class='text-center'> ";
+								echo " <ul class='pagination'> ";
+								    echo " <li><a href='?pagina=".$i."'>".$i."</a>  </li> ";
+								            
+								    echo " </ul> ";
+							echo "  </div>";
+
+
+						}; ?>
 		            </div>
 		        </div>
 		            <!-- Blog Sidebar Column -->
