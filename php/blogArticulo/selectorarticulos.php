77a567f442cc8559b0a55914c94db169d3769b85
jorgejaico <93186.joan23@fje.edu>
2016-05-28 13:52:39 +0200

    arreglar twitter blogs y articulos

--- php/blogArticulo/selectorarticulos.php
+++ php/blogArticulo/selectorarticulos.php
@@ -264,7 +264,7 @@
 			<a href="https://twitter.com/intent/tweet?screen_name=Moda_Clamm" class="twitter-mention-button" data-lang="es" data-size="large">Tweet a @Moda_Clamm</a>
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 			<br><br>
-			<a href="https://twitter.com/rockmoda" class="twitter-follow-button" data-show-count="false" data-lang="es" data-size="large" data-show-screen-name="false">Seguir a @Moda_Clamm</a>
+			<a href="https://twitter.com/Moda_Clamm" class="twitter-follow-button" data-show-count="false" data-lang="es" data-size="large" data-show-screen-name="false">Seguir a @Moda_Clamm</a>
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 			<br><br>
 		                </div>
