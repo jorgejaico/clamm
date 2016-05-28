2e8a2d8235e8a36cd298ae19f61b91a5d385816a
SanchezEric <8706.joan23@fje.edu>
2016-05-28 02:36:55 +0200

    Enlaces footer

--- php/headerfooter/footer.php
+++ php/headerfooter/footer.php
@@ -3,10 +3,10 @@
 			<div class="container">
 				<div class="text-center height-contact-element">
 					<ul class="list-unstyled list-inline list-social-icons">
-						<li class="active"><a href="#"><i class="fa fa-facebook social-icons"></i></a></li>
-						<li><a href="#"><i class="fa fa-twitter social-icons"></i></a></li>
-						<li><a href="#"><i class="fa fa-google-plus social-icons"></i></a></li>
-						<li><a href="#"><i class="fa fa-instagram social-icons"></i></a></li>
+						<li class="active"><a href="https://www.facebook.com/Clamm-Moda-132898603781907/?success=0&errornum=1357038"><i class="fa fa-facebook social-icons"></i></a></li>
+						<li><a href="https://twitter.com/search?q=Moda_Clamm&ref_src=twsrc%5Etfw"><i class="fa fa-twitter social-icons"></i></a></li>
+						<li><a href="https://plus.google.com/u/0/103841515779878158663"><i class="fa fa-google-plus social-icons"></i></a></li>
+						<li><a href="https://www.instagram.com/clamm.moda/"><i class="fa fa-instagram social-icons"></i></a></li>
 					</ul>
 				</div>
 				<div class="row myfooter">
